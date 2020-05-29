469e9ed95aad598be1318dda1be21c893b729df0
xCroa <44088680+xCroa@users.noreply.github.com>
2020-05-28 21:30:50 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ except ImportError:
     from queue import Queue
 
 app = Flask(__name__)
-TOKEN = os.environ['PP_BOT_TOKEN']  # put your token in heroku app as environment variable
+TOKEN = '1023777751:AAE6YfdkFZH9rQgEQC1mAc-0YUWzMlXmw6E'#os.environ['PP_BOT_TOKEN']  # put your token in heroku app as environment variable
 SECRET = '/bot' + TOKEN
 URL = '' #  paste the url of your application
 
