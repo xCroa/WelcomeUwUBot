e5901dc3c128716611be9b1222f0063e271b47ef
xCroa <44088680+xCroa@users.noreply.github.com>
2020-05-29 00:08:51 -0300

    changin library

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn -b 0.0.0.0:$PORT bot:app
+web: python3 bot.py
--- bot.py
+++ bot.py
@@ -1,34 +1,66 @@
-from flask import Flask, request
-import telepot
-import time
+import logging
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import os
 
-try:
-    from Queue import Queue
-except ImportError:
-    from queue import Queue
 
-app = Flask(__name__)
-TOKEN = '1023777751:AAE6YfdkFZH9rQgEQC1mAc-0YUWzMlXmw6E'#os.environ['PP_BOT_TOKEN']  # put your token in heroku app as environment variable
-SECRET = '/bot' + TOKEN
-URL = 'https://welcomeuwubot.herokuapp.com' #  paste the url of your application
 
-UPDATE_QUEUE = Queue()
-BOT = telepot.Bot(TOKEN)
 
-def on_chat_message(msg):
-    content_type, chat_type, chat_id = telepot.glance(msg)
-    BOT.sendMessage(chat_id, 'hello!')
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
 
-BOT.message_loop(on_chat_message, source=UPDATE_QUEUE)  # take updates from queue
+logger = logging.getLogger(__name__)
+TOKEN = '1023777751:AAE6YfdkFZH9rQgEQC1mAc-0YUWzMlXmw6E'
+PORT = int(os.environ.get('PORT', 5000))
 
-@app.route(SECRET, methods=['GET', 'POST'])
-def pass_update():
-    print('asd')
-    UPDATE_QUEUE.put(request.data)  # pass update to bot
-    return 'OK'
+# Define a few command handlers. These usually take the two arguments update and
+# context. Error handlers also receive the raised TelegramError object in error.
+def start(update, context):
+    #"""Send a message when the command /start is issued."""
+    update.message.reply_text('Hi!')
 
-while True:
-    time.sleep(1)
+def help(update, context):
+    #"""Send a message when the command /help is issued."""
+    update.message.reply_text('Help!')
 
-BOT.setWebhook(URL + SECRET) # unset if was set previously
+def echo(update, context):
+    #"""Echo the user message."""
+    update.message.reply_text(update.message.text)
 
+def error(update, context):
+    #"""Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+def main():
+    #"""Start the bot."""
+    # Create the Updater and pass it your bot's token.
+    # Make sure to set use_context=True to use the new context based callbacks
+    # Post version 12 this will no longer be necessary
+    updater = Updater(TOKEN, use_context=True)
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+
+    # on noncommand i.e message - echo the message on Telegram
+    dp.add_handler(MessageHandler(Filters.text, echo))
+
+    # log all errors
+    dp.add_error_handler(error)
+
+    # Start the Bot
+    updater.start_webhook(listen="0.0.0.0",
+                          port=int(PORT),
+                          url_path=TOKEN)
+    updater.bot.setWebhook('https://welcomeuwubot.herokuapp.com/' + TOKEN)
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
