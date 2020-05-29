6e76eb768e3fddad01754d5663c326319ba698fd
xCroa <44088680+xCroa@users.noreply.github.com>
2020-05-29 05:20:16 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,10 +21,11 @@ def start(update, context):
     update.message.reply_text('Este bot envia um gif de bem vindo quando o grupo ganha novos integrantes!')
 
 def echo(update, context):
+    print('OI')
     bot = context.bot
     url = '(https://i.imgur.com/lw7Z1mL.mp4)'#helpers.create_deep_linked_url(bot.get_me().username, USING_ENTITIES)        
     user_id_mention = '(tg://user?id=' + str(update.message.new_chat_members[0].id) + ')'
-    text = 'Welcome, ['+ update.message.user.first_name +']' + user_id_mention + '[UwU]' + url + '!'
+    text = 'Welcome, ['+ update.message.new_chat_members[0].first_name +']' + user_id_mention + '[UwU]' + url + '!'
     update.message.reply_text(text, parse_mode=ParseMode.MARKDOWN)    
 
 def main():
