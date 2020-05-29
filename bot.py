77f290a8fbac563b5d81dfc939d5cbf2da759c7b
xCroa <44088680+xCroa@users.noreply.github.com>
2020-05-29 05:17:46 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -23,7 +23,7 @@ def start(update, context):
 def echo(update, context):
     bot = context.bot
     url = '(https://i.imgur.com/lw7Z1mL.mp4)'#helpers.create_deep_linked_url(bot.get_me().username, USING_ENTITIES)        
-    user_id_mention = '(tg://user?id=' + update.message.user.id + ')'
+    user_id_mention = '(tg://user?id=' + str(update.message.new_chat_members[0].id) + ')'
     text = 'Welcome, ['+ update.message.user.first_name +']' + user_id_mention + '[UwU]' + url + '!'
     update.message.reply_text(text, parse_mode=ParseMode.MARKDOWN)    
 
